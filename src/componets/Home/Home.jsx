8e288589bd59693fa05d3c6f62ad08c1664581de
Md-Abdur-Razzak <razzak445444@gmail.com>
2023-10-18 00:37:32 +0600

    Technology and Electronics Banner Section

--- src/componets/Home/Home.jsx
+++ src/componets/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            Ima a  home compj
+          <Banner></Banner>
         </div>
     );
 };
